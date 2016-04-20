ea4d4f635b32bdfcd4bfce2fc7a1f9069b4d1240
Carsten Csiky <carsten.csiky@gmail.com>
2016-04-20 22:03:50 +0200

    basic version working & cleaned up

---
+++ loginLogin.js
@@ -0,0 +1,26 @@
+(function() {
+  var submitButton  = document.querySelector('#sbmt')
+  var nameInput     = document.querySelector('input[name="j_username"]');
+  var passwdInput   = document.querySelector('input[name="j_password"]');
+
+
+  if (!nameInput || !passwdInput || !submitButton) {
+    //out of luck
+    return;
+  }
+
+  function checkIfIsCorrect() {
+    if(nameInput.value.length == 5 && passwdInput.value != '') {
+      submitButton.click();
+    }
+  }
+
+  nameInput.addEventListener('change', function() {
+    checkIfIsCorrect();
+  })
+
+  passwdInput.addEventListener('change', function() {
+    checkIfIsCorrect();
+  })
+
+}())
---
+++ loginSelect.js
@@ -0,0 +1,4 @@
+(function(){
+  var login = document.querySelector('input[value="Mit KIT-Account anmelden"]');
+  login.click();
+}())
